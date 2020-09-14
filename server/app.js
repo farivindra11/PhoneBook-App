8aaec95597ed4b4e9555b872e0c6c03baa69bd7d
fariv indra <farivind@gmail.com>
2020-09-15 03:01:37 +0700

    congig firebase

--- server/app.js
+++ server/app.js
@@ -1,12 +1,25 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const firebase = require('firebase')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const config = {
+    apiKey: "AIzaSyAme8EcznDTo_MKkY9TQaMa_XXnuEq0dDs",
+    authDomain: "rubicamp-go-12890.firebaseapp.com",
+    databaseURL: "https://rubicamp-go-12890.firebaseio.com",
+    projectId: "rubicamp-go-12890",
+    storageBucket: "rubicamp-go-12890.appspot.com",
+    messagingSenderId: "612307324185"
+  };
+firebase.initializeApp(config);
 
-var app = express();
+
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());

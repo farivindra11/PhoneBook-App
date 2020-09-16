62a61d35ecb551870a6df9adde1f9a72e49d3772
fariv indra <farivind@gmail.com>
2020-09-16 20:53:54 +0700

    search done

--- server/services/index.js
+++ server/services/index.js
@@ -1,15 +1,21 @@
 const firebase = require("firebase");
 
-const getUsers = () => {
+const getUsers = (offset, limit) => {
+
   const userReference = firebase.database().ref("/Users/");
+
   return (new Promise((resolve, reject)=>{
     userReference.on("value", function(snapshot) {
       const folders = snapshot.val();
+
       if (folders === null) {
         resolve([]);
       }else{
         const data = Object.keys(folders).map(o => Object.assign({ id: o }, folders[o]));
-        resolve(data);
+        const result = data.length;
+        const list = data.splice(offset, limit);
+
+        resolve({result, list});
       }
       userReference.off("value");
     }, (errorObject) => {
@@ -18,6 +24,51 @@ const getUsers = () => {
     });
   }));
 }
+
+// search 
+const serachUsers = (name, phone, offset, limit) => {
+
+  const regName = new RegExp(name, 'ig')
+  const regPhone = new RegExp(phone, 'g')
+  const phoneReference = firebase.database().ref("/Users/");
+
+  return (new Promise((resolve, reject) => {
+
+    phoneReference.on("value", function (snapshot) {
+      const folders = snapshot.val();
+
+      if (folders === null) {
+        resolve([]);
+      } else {
+
+        const rows = Object.keys(folders).map(o => Object.assign({ id: o }, folders[o])).filter(item => {
+
+          if (name && phone) {
+            return item.Name.match(regName) && item.Phone.match(regPhone)
+          }else if(name) {
+            return item.Name.match(regName)
+          }else if(phone) { 
+            return item.Phone.match(regPhone)
+          }else{
+            
+          return false
+            
+          } 
+        })
+
+        const dataLength = rows.length
+        const listData = rows.splice(offset, limit)
+
+        resolve({ listData, dataLength });
+      }
+      phoneReference.off("value");
+    }, (errorObject) => {
+      console.log("The read failed: " + errorObject.code);
+      reject("The read failed: " + errorObject.code);
+    });
+  }));
+}
+
 
 //Create new instance
 const createUser = (user) => {
@@ -64,4 +115,4 @@ const deleteUser = (user) => {
   }));
 }
 
-module.exports = {getUsers, createUser, updateUser, deleteUser}
+module.exports = {getUsers, createUser, updateUser, deleteUser, serachUsers}

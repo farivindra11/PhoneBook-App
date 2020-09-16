83a3a3a4b67e2778053894bd142e4b799afd1d3e
fariv indra <farivind@gmail.com>
2020-09-16 21:01:56 +0700

    reSchema graphql

--- server/graphql/types/user.js
+++ server/graphql/types/user.js
@@ -2,7 +2,7 @@ var GraphQLObjectType = require('graphql').GraphQLObjectType;
 var GraphQLNonNull = require('graphql').GraphQLNonNull;
 var GraphQLID = require('graphql').GraphQLID;
 var GraphQLString = require('graphql').GraphQLString;
-var GraphQLInteger = require('graphql').GraphQLInteger;
+
 
 // User Type
 exports.userType = new GraphQLObjectType({

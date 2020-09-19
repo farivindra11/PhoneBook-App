ceac1df78ff05192c50b07f2bd323d03c99d570c
fariv indra <farivind@gmail.com>
2020-09-20 03:10:45 +0700

    box inProgress

--- client/src/containers/UserBox.js
+++ client/src/containers/UserBox.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 import UserList from './UserList';
 import { connect } from 'react-redux';
-
+import Jumbotron from './Jumbotron'
+import AddForm from './AddForm';
 class UserBox extends Component {
-    render () {
-        return (
-           
-                <div className="card-body">
-                  <UserList />
-                </div>
-               
-        )
-    }
+  render() {
+    return (
+      <div className="jumbo">
+      <Jumbotron />
+      <div className="container">
+        <div className="main-container">
+        <div className="card-body">
+           {this.props.stateFromMaps && <AddForm />}
+            </div>
+        <div className="card-body">
+          <UserList />
+          </div>
+        </div>
+      </div>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = ({ users }) => {
-    const {isActive}= users
-    return { stateFromMaps: isActive } 
-  }
-  
-  export default connect(
-    mapStateToProps
-  )(UserBox)
+  const { isActive } = users
+  return { stateFromMaps: isActive }
+}
+
+export default connect(
+  mapStateToProps
+)(UserBox)

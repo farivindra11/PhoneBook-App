5eb92a86616afce17b76b74ee5b8a1d2c3c88854
fariv indra <farivind@gmail.com>
2020-09-21 02:55:58 +0700

    Done

--- client/src/containers/UserBox.js
+++ client/src/containers/UserBox.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
-import UserList from './UserList';
 import { connect } from 'react-redux';
-import Jumbotron from './Jumbotron'
+import Jumbotron from './Jumbotron';
+import UserList from './UserList';
 import AddForm from './AddForm';
+import SearchForm from './SearchForm';
+import Pagination from './Pagination';
+
 class UserBox extends Component {
   render() {
     return (
       <div className="jumbo">
-      <Jumbotron />
-      <div className="container">
-        <div className="main-container">
-        <div className="card-body">
-           {this.props.stateFromMaps && <AddForm />}
+        <Jumbotron />
+        <div className="container">
+          <div className="main-container">
+            <div className="card-body">
+              <div className="table-wrapper">
+                <div className="card-body">
+                  {this.props.stateFromMaps && <AddForm />}
+                  <br />
+                  <SearchForm />
+                </div>
+                <div className="card-body">
+                  <UserList />
+                </div>
+                <Pagination />
+              </div>
             </div>
-        <div className="card-body">
-          <UserList />
           </div>
         </div>
-      </div>
       </div>
     )
   }

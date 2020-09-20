34ba6204a1e5e6698e616b7cf3dc4f5e2c18181a
fariv indra <farivind@gmail.com>
2020-09-21 02:49:08 +0700

    User done

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -11,7 +11,7 @@ const User = (props) => {
       <td>{props.user}</td>
       <td>
         {props.added && <button type="button" className="btn btn-outline-danger del" onClick={props.onDelete} >  <i className="fa fa-trash"></i> </button>}
-        {!props.added && <button type="button" className="btn btn-outline-warning del" onClick={props.resend}><i className="fa fa-repeat"></i> Resend </button>}
+        {!props.added && <button type="button" className="btn btn-outline-warning del" onClick={props.resend}><i className="fa fa-repeat"></i> </button>}
         <button type="button" className="btn btn-outline-success" onClick={props.onEdit}> <i className="fa fa-pencil-square"></i></button>
       </td>
     </tr>
